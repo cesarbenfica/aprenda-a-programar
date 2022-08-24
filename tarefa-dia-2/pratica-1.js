9ec0ffb84531de3cafb8667c9868d2830b2686ca
César Benfica <cesar.benfica.450@hotmail.com>
2022-08-23 22:54:47 -0300

    Término Prática 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "cesar.benfica.450@hotmail.com"
 let profissao = "Engenheiro Civil"
 let idade = 26
 let lugar = "Hawai"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
+console.log(nome , email , profissao , idade , lugar)
