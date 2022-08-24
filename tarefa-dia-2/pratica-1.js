7f03826db7c39bde3b1e1ffc0422c3368eb7af43
César Benfica <cesar.benfica.450@hotmail.com>
2022-08-23 23:23:18 -0300

    Desafio Prática 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,3 +9,12 @@ console.log(profissao)
 console.log(idade)
 console.log(lugar)
 console.log(nome , email , profissao , idade , lugar)
+
+//Desafio
+
+console.log('Olá,' , nome , '!' , 'Somos aqui da agência de turismo\n' +
+'Viagem dos Sonhos. Estamos te escrevendo este email, pois\n' +
+'acabamos de confirmar as compras das passagens aéreas para\n' +
+lugar , '.' , 'Confirma pra gente alguns dados? A sua idade é' , idade , ',\n' +
+'você é' , profissao , 'e o seu email é' , email , '?\n' +
+'Ficamos no aguardo. Muito obrigado e boa viagem! :D')
